16ac2f2c2f65fbcf2fe8a51901ed2dd2f32f2d84
cxcongcong <cxcongcong@gmail.com>
2022-06-05 02:12:10 +1000

    navbar update

--- src/components/Icons/NavbarButton.js
+++ src/components/Icons/NavbarButton.js
@@ -1,18 +1,19 @@
 import "./NavBarButton.css";
 
 function NavbarButton(props) {
+    const sublist = <div className="navbtn-sublist">
+                        <div className="hi"><NavbarButton name={props.subitem1}/></div>
+                        <NavbarButton name={props.subitem2} top="90px"/>
+                        <NavbarButton name={props.subitem3} top="135px"/>
+                    </div>;
     return (
-        <div className="a" style={{position:'absolute', top:props.top, right: props.right}}>
-            <div className="b">{props.name}</div>
-            <div className="c">
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-            </div>
+        <div className="navbtn-container" style={{position:'absolute', top:props.top, right: props.right}}>
+            <div className="navbtn-btn">{props.name}</div>
+            {props.sublist?sublist:''}
         </div>
     )
+
+    
 }
 
 export default NavbarButton;
