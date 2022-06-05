9019fd089af227a57c5d93e85a2d539bfeb892c9
cxcongcong <cxcongcong@gmail.com>
2022-06-05 12:45:33 +1000

    navigation bar button updated

--- src/components/Icons/NavbarButton.js
+++ src/components/Icons/NavbarButton.js
@@ -1,19 +1,23 @@
 import "./NavBarButton.css";
 
 function NavbarButton(props) {
-    const sublist = <div className="navbtn-sublist">
-                        <div className="hi"><NavbarButton name={props.subitem1}/></div>
-                        <NavbarButton name={props.subitem2} top="90px"/>
-                        <NavbarButton name={props.subitem3} top="135px"/>
+    let sublist = '';
+    if (props.sublist) {
+        const sublist_items_name = props.sublist.split(';');
+        const sublist_items_div = sublist_items_name.map((name,i) =>
+            <div className="navbtn-sublist-btn"><NavbarButton name={name} top={45+45*i}/></div>
+        )
+        sublist = <div className="navbtn-sublist">
+                        {sublist_items_div}
                     </div>;
+    }
+
     return (
         <div className="navbtn-container" style={{position:'absolute', top:props.top, right: props.right}}>
             <div className="navbtn-btn">{props.name}</div>
-            {props.sublist?sublist:''}
+            {sublist}
         </div>
     )
-
-    
 }
 
 export default NavbarButton;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,8 +12,8 @@ function Navbar() {
             </div>
             <div className="navbar-right-part">
                 <NavbarButton name="ABOUT ME" top="0px" right="310px"/>
-                <NavbarButton name="RESUME" top="0px" right="220px" sublist="true" subitem1="SKILLS" subitem2="EXPERIENCE" subitem3="EDUCATION"/>
-                <NavbarButton name="BLOGS" top="0px" right="140px" sublist="true" subitem1="Content" subitem2="The art of..." subitem3="How to..."/>
+                <NavbarButton name="RESUME" top="0px" right="220px" sublist="SKILLS;EXPERIENCE;EDUCATION"/>
+                <NavbarButton name="BLOGS" top="0px" right="140px" sublist="Content;The art of...;How to..."/>
                 <NavbarButton name="CONTACT" top="0px" right="60px"/>
             </div>
         </div>
