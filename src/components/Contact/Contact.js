bb063181982519ad19a3f07a1b8286379b80bbc0
cxcongcong <cxcongcong@gmail.com>
2022-06-09 15:07:37 +1000

    contact page updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,15 @@
+import './Contact.css';
+import Square from '../Icons/Square';
+
 function Contact() {
     return (
-        <div>
-            <div style={{fontSize:'380px'}}>Contact</div>
-            <div style={{fontSize:'380px'}}>HI</div>
+        <div className='contact'>
+            <div className='contact-heading'>
+                <Square bgcolor="blue" left="140px" top="18px"/> Let's talk
+            </div>
+            <div className='contact-table'>
+
+            </div>
         </div>
     )
 }
