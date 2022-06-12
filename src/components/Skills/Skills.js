3fafd5dc3c6def42b27cb3f30985903bc220fae9
cxcongcong <cxcongcong@gmail.com>
2022-06-12 12:25:59 +1000

    update skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,8 +1,20 @@
+import '../Icons/Resume.css';
+import Square from '../Icons/Square';
+
 function Skills() {
     return (
-        <div>
-            <div style={{fontSize:'380px'}}>Skills</div>
-            <div style={{fontSize:'380px'}}>HI</div>
+        <div className="resume">
+            <div className='resume-context'>
+                <div className="resume-heading">Professional skillset</div>
+                <div className="resume-info-block">
+                    <div className='resume-skills-info-block'>
+                        <div className='resume-skills-heading'>Soft skills</div>
+                        <div className='resume-skills-context' style={{position:'relative'}}>
+                            <Square top="10px" left="10px" bgcolor="blue"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
