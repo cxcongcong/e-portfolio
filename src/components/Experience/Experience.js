9f4dbddab0933837b0e28856b392af622988f97a
cxcongcong <cxcongcong@gmail.com>
2022-06-14 01:49:47 +1000

    update experience page

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,20 @@
+import '../Icons/Resume.css';
+import ExperienceInfoBlock from './ExperienceInfoBlock';
+import { experience_1, experience_2, experience_3, experience_4 } from './ExperienceData.js';
+
 function Experience() {
     return (
-        <div>
-            <div style={{fontSize:'380px'}}>Experience</div>
-            <div style={{fontSize:'380px'}}>HI</div>
+        <div className="resume">
+            <div className='resume-context'>
+                <div className="resume-heading">Experience</div>
+                <div className="resume-info-block resume-info-border">
+                    <ExperienceInfoBlock info={experience_1}/>
+                    <ExperienceInfoBlock info={experience_2}/>
+                    <ExperienceInfoBlock info={experience_3}/>
+                    <ExperienceInfoBlock info={experience_4}/>
+                </div>
+                <div className='resume-ending'></div>
+            </div>
         </div>
     )
 }
---
+++ src/components/Experience/ExperienceData.js
@@ -0,0 +1,38 @@
+export const experience_1 = {
+    time: '2021.12 - 2022.5',
+    career: 'Frontend developer',
+    company: 'Pulp You Media',
+    location: 'Sydney NSW Australia',
+    description: ['Collaborated with other interns and had a weekly meeting with teammates to guarantee the development process.',
+                'Developed websites using HTML5, JavaScript, CSS, and React.',
+                'Enhanced user experience and accomplish webpage objectives by creating site structure and navigation.',
+                'Implemented enhancements that improved web functionality and responsiveness.']
+};
+
+export const experience_2 = {
+    time: '2022.1 - Present',
+    career: 'Pastry & Cashier',
+    company: 'Meet Fresh',
+    location: 'Burwood Sydney NSW Australia',
+    description: ['Negotiated with clients and implemented financial management.',
+                'Serving clients with good attitude.',
+                'Making desserts.']
+};
+
+export const experience_3 = {
+    time: '2021.10 - 2022.1',
+    career: 'Hospitality Assistant',
+    company: 'The Eight',
+    location: 'Haymarket Sydney NSW Australia',
+    description: ['Negotiated with clients and implemented financial management.',
+                'Ensured the smooth running of the day-to-day hotel operation.']
+};
+
+export const experience_4 = {
+    time: '2021.10 - 2022.1',
+    career: 'Barista & Cashier',
+    company: 'Koomi',
+    location: 'Strathfield Sydney NSW Australia',
+    description: ['Negotiated with clients and implemented financial management.',
+                'Making drinks.']
+};
