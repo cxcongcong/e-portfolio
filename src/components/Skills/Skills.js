a2df2bebf914f336dae4a56f5ebaf12f8be2ba98
cxcongcong <cxcongcong@gmail.com>
2022-06-14 00:57:24 +1000

    update skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,20 +1,18 @@
 import '../Icons/Resume.css';
-import Square from '../Icons/Square';
+import { soft_skills, languages, programming } from './SkillsData';
+import SkillsInfoBlock from './SkillsInfoBlock';
 
 function Skills() {
     return (
         <div className="resume">
             <div className='resume-context'>
                 <div className="resume-heading">Professional skillset</div>
-                <div className="resume-info-block">
-                    <div className='resume-skills-info-block'>
-                        <div className='resume-skills-heading'>Soft skills</div>
-                        <div className='resume-skills-context' style={{position:'relative'}}>
-                            <Square bgcolor="blue" display="inline-block"/>a
-                            <Square bgcolor="blue" display="inline-block"/>b
-                        </div>
-                    </div>
+                <div className="resume-info-block resume-info-shadow">
+                    <SkillsInfoBlock info={soft_skills}/>
+                    <SkillsInfoBlock info={languages}/>
+                    <SkillsInfoBlock info={programming}/>
                 </div>
+                <div className='resume-ending'></div>
             </div>
         </div>
     )
