ee3a0e2f39a9abe9bb7e56a4f5dd9395c8f347ad
cxcongcong <cxcongcong@gmail.com>
2022-06-16 16:10:00 +1000

    update project page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,7 +1,7 @@
 import '../Icons/Resume.css';
 import './Project.css';
 import ProjectInfoBlock from './ProjectInfoBlock';
-import { project_1, project_2 } from './ProjectData';
+import { project_1, project_2, project_3, project_4 } from './ProjectData';
 import { useState } from 'react';
 
 function Project() {
@@ -12,8 +12,10 @@ function Project() {
                 <div className="resume-heading">
                     Projects
                     <div>
-                        <div className='project-switch-point' onClick={()=>{setProject(project_1)}}>M</div>
-                        <div className='project-switch-point' onClick={()=>{setProject(project_2)}}>O</div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_1)}}>1</div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_2)}}>2</div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_3)}}>3</div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_4)}}>4</div>
                     </div>
                 </div>
                 <div className="resume-info-block resume-info-border">
--- src/components/Project/ProjectData.js
+++ src/components/Project/ProjectData.js
@@ -1,5 +1,7 @@
 import myrecipe from '../../images/projects/myrecipe.jpg';
 import odbc from '../../images/projects/odbc.jpg';
+import geography from '../../images/projects/geography.png';
+import flaskpic from '../../images/projects/flask.jpg';
 
 export const project_1 = {
     name: 'My recipes',
@@ -13,6 +15,7 @@ export const project_1 = {
             'Added error prevention and feedback to improve user experience.',
             'Welcome to have a glimpse of this project: https://github.com/cxcongcong/graduate_program']
 }
+
 export const project_2 = {
     name: 'ODBC',
     pic: odbc,
@@ -21,4 +24,22 @@ export const project_2 = {
             'Developed websites using HTML5, JavaScript, CSS and React.',
             'Enhanced user experience and accomplish webpage objectives by creating site structure and navigation.',
             'Implemented enhancements that improved web functionality and responsiveness.']
+}
+
+export const project_3 = {
+    name: 'Geographic data processing',
+    pic: geography,
+    tools: ['Python'],
+    desc: ['Data ingestion, cleaning manipulation and Visualization.',
+            'Rank the countries in dataframe based on the distance from the first case of COVID.',
+            'Find the average foreign direct investment for each income class.',
+            'Plot a scatter plot based on its latitude and longitude, and ink each country based on its continent .']
+}
+
+export const project_4 = {
+    name: 'Data Service (REST API)',
+    pic: flaskpic,
+    tools: ['Python','Pandas','flask'],
+    desc: ['Develop a Flask-Restx data service that allows a client to read and store some TV Shows, and allow the consumers to access the data through a REST API.',
+            'Deal with cases such as invalid titles and invalid attempts to use the endpoint.']
 }
