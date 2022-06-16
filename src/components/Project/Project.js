77dc7a005c4ee2e7ca0c27b86055e600ecae7a81
cxcongcong <cxcongcong@gmail.com>
2022-06-16 13:31:56 +1000

    update project

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,14 +1,23 @@
 import '../Icons/Resume.css';
+import './Project.css';
 import ProjectInfoBlock from './ProjectInfoBlock';
-import { project_1 } from './ProjectData';
+import { project_1, project_2 } from './ProjectData';
+import { useState } from 'react';
 
 function Project() {
+    const [ project, setProject ] = useState(project_1);
     return (
         <div className="resume">
             <div className='resume-context'>
-                <div className="resume-heading">Projects</div>
+                <div className="resume-heading">
+                    Projects
+                    <div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_1)}}>M</div>
+                        <div className='project-switch-point' onClick={()=>{setProject(project_2)}}>O</div>
+                    </div>
+                </div>
                 <div className="resume-info-block resume-info-border">
-                    <ProjectInfoBlock info={project_1}/>
+                    <ProjectInfoBlock info={project}/>
                 </div>
                 <div className='resume-ending'></div>
             </div>
--- src/components/Project/ProjectData.js
+++ src/components/Project/ProjectData.js
@@ -1,8 +1,10 @@
 import myrecipe from '../../images/projects/myrecipe.jpg';
+import odbc from '../../images/projects/odbc.jpg';
+
 export const project_1 = {
     name: 'My recipes',
     pic: myrecipe,
-    tools: ['Bootstrap','jQuery'],
+    tools: ['Bootstrap','jQuery','JavaScript','CSS','HTML'],
     desc: ['Collaborated with 4 team members analysing user requirements and designing the prototype of My Recipes website.',
             'Utilized Jira to manage team corporation as well as the process of project.',
             'Designed the layout of proposal and wrote proposal to meet the requirements.',
@@ -10,4 +12,13 @@ export const project_1 = {
             'Checked and fixed bugs to keep the results of functions consistent between different web pages.',
             'Added error prevention and feedback to improve user experience.',
             'Welcome to have a glimpse of this project: https://github.com/cxcongcong/graduate_program']
+}
+export const project_2 = {
+    name: 'ODBC',
+    pic: odbc,
+    tools: ['React','JavaScript','CSS'],
+    desc: ['Collaborated with other interns and had a weekly meeting with teammates to guarantee the development process.',
+            'Developed websites using HTML5, JavaScript, CSS and React.',
+            'Enhanced user experience and accomplish webpage objectives by creating site structure and navigation.',
+            'Implemented enhancements that improved web functionality and responsiveness.']
 }
